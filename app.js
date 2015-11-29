4ce4dbcb0a992d0963c3e5d85786d4f81a62e56b
MichaelMillward <michael.millward.nz@gmail.com>
2015-11-29 18:14:51 +1300

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var SC = require('node-soundcloud');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// Initialize client
+SC.init({
+  id: 'e8a4c59ab006807fbd0b5fda265802d2',
+  secret: '95764ea9b46bca2f63397a9eb26bd2c8'
+});
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use('/users', users);
+
+app.use('/soundcloud', function(req, res){
+  var url = SC.getConnectUrl();
+
+  res.writeHead(301, {
+    'Location': url
+  });
+
+  res.end();
+});
+
+app.use('/redirect', function(req, res){
+
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
